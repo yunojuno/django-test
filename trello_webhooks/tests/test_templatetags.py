13795072150d36af322ccbe10e2bae4909d61189
Kanstantsin Nikitsin <kanstantsin.nikitsin@itechart-group.com>
2017-04-07 17:12:26 +0300

    Add attachment rendering test

--- trello_webhooks/tests/test_templatetags.py
+++ trello_webhooks/tests/test_templatetags.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.template import Context, Template
 from django.test import TestCase
 
 from trello_webhooks.settings import TRELLO_API_KEY
-from trello_webhooks.templatetags.trello_webhook_tags import (
-    trello_api_key,
-    trello_updates
-)
+from trello_webhooks.templatetags.trello_webhook_tags import (trello_api_key,
+                                                              trello_updates)
 
 
 class TemplateTagTests(TestCase):
@@ -28,3 +27,21 @@ class TemplateTagTests(TestCase):
             trello_updates(new, old),
             {'pos': (1, None)}
         )
+
+
+class TestHipchatTags(TestCase):
+    TEMPLATE = Template("{% load hipchat %} {{ attachment|render_attachment }}")
+
+    def test_render_attachment(self):
+        attachment = {'url': 'http://test',
+                      'name': 'test-name',
+                      'content_type': 'test'}
+        rendered = self.TEMPLATE.render(Context({'attachment': attachment}))
+
+        self.assertIn('test-name', rendered)
+
+        attachment = {'url': 'http://test',
+                      'name': 'test-name',
+                      'content_type': 'video/webm'}
+        rendered = self.TEMPLATE.render(Context({'attachment': attachment}))
+        self.assertIn('video', rendered)
