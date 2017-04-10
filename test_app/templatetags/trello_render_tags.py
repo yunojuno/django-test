7b84fc46ab858df05ce53c363fd2f2f76e8bb361
Evgeny Lychkovsky <evgeny.lychkovsky@itechart-group.com>
2017-04-10 15:35:31 +0300

    Content type specific rendering for actions' attachment

---
+++ test_app/templatetags/trello_render_tags.py
@@ -0,0 +1,25 @@
+from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def render_card_attachment(attachment):
+    content_type = attachment.get('mimeType') or 'unknown'
+
+    if content_type.startswith('image/'):
+        # show image title with a link and image itself below
+        attachment_template = (
+            '"<strong><a href="{url}">{name}</a></strong>"'
+            '<br/><a href="{url}"><img src="{url}" alt="{name}"/></a>'
+        )
+    else:
+        attachment_template = '"<strong><a href="{url}">{name}</a></strong>"'
+
+    return mark_safe(attachment_template.format(
+        url=conditional_escape(attachment.get('url', '')),
+        name=conditional_escape(attachment.get('name', '')),
+    ))
