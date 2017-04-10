406be5851afe8f13c0fad994d927e7c06419c839
Nizovtsova, Mariya <m.nizovtsova@itransition.com>
2017-04-10 18:12:05 +0300

    Add new template for attachment and filter for rendering

---
+++ test_app/templatetags/hipchat_tags.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter
+def render_attachment(attachment_data):
+    """Render attachment to correct html.
+    If attachment data has an image type, template will be formatted with img tag.
+    Otherwise if will be formatted as a simple url"""
+    content_type = attachment_data.get('content_type', '')
+    attachment_is_image = content_type.split('/').lower() == 'image'
+
+    formatted_string = '<a href="{{url}}"><img src={{url}}></a>' if attachment_is_image \
+        else '<a href="{{url}}">{{name}}</a>'
+
+    return mark_safe(formatted_string.format(**attachment_data))
